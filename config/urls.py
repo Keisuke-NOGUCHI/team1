8331b0b395e76fc2b463cfdf81aaf51c9b65ff5a
Keisuke-NOGUCHI <s1f102001805@iniad.org>
2022-06-26 01:27:40 +0900

    app01
    topページの作成

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import app01.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('top/', app01.views.root),
+    path('', app01.views.top, name='top'),
+    path('app01/pattern/<username>/', app01.views.pattern, name='pattern'),
+    path('app01/param/', app01.views.param, name='param'),
 ]
