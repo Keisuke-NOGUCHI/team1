8bce13aa02fcc72fa5ad9fdc948191083ee8006f
Keisuke-NOGUCHI <s1f102001805@iniad.org>
2022-10-06 16:33:30 +0900

    a

--- app01/views.py
+++ app01/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import Http404
-from matplotlib import image
-from numpy import imag
 from app01.models import Article, Comment
 from .forms import Image
 
@@ -50,7 +48,7 @@ def index(request):
 		article = Article(title=request.POST["title"],body=request.POST["text"])
 		#article.image=request.POST['image']
 		if form.is_valid():
-			article.image=request.FILES.get('image')
+			article.image=request.FILES['image']
 		else:
 			article.image=False
 		article.save()
